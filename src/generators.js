edcb2de07c9587aefe6809999ecbadc519565ac1
Jeff Jagoda <jeff.jagoda@lifeomic.com>
2018-08-22 12:40:22 -0400

    TravisCI

--- src/generators.js
+++ src/generators.js
@@ -2,7 +2,7 @@ exports.kinesisRecord = (payload) => ({
   kinesis: {
     data: Buffer.from(JSON.stringify(payload)).toString('base64')
   }
-});;
+});
 
 exports.streamEvent = (events) => ({
   Records: [].concat(events)
