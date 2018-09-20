644a823658901083fd58ed40fcb777cf7318be92
Jeff Jagoda <jeff.jagoda@lifeomic.com>
2018-09-20 09:20:31 -0400

    AWS scheduled event support

--- test/generators.test.js
+++ test/generators.test.js
@@ -1,4 +1,6 @@
 const test = require('ava');
+
+const { DateTime } = require('luxon');
 const { generators } = require('..');
 
 test('generating a synthetic kinesis event encodes the payload object', (test) => {
@@ -25,4 +27,43 @@ test('generating a stream event from multiple payloads creates a record for each
 
   const event = generators.streamEvent(payloads);
   test.deepEqual(event, { Records: payloads });
+});
+
+test('generating a scheduled event uses the current time and no schedule resource by default', (test) => {
+  const start = DateTime.utc();
+  const event = generators.scheduledEvent();
+  const time = DateTime.fromISO(event.time);
+
+  const end = DateTime.utc();
+  test.is(event['detail-type'], 'Scheduled Event');
+  test.is(event.source, 'aws.events');
+  test.true(time.isValid);
+  test.true(time >= start);
+  test.true(time <= end);
+  test.deepEqual(event.resources, []);
+});
+
+test('scheduled event time can be customized', (test) => {
+  const time = DateTime.utc().toISO();
+  const event = generators.scheduledEvent({ time });
+
+  test.is(event['detail-type'], 'Scheduled Event');
+  test.is(event.source, 'aws.events');
+  test.is(event.time, time);
+  test.deepEqual(event.resources, []);
+});
+
+test('scheduled event resource can be customized', (test) => {
+  const schedule = 'arn:aws:events:us-east-1:123456789012:rule/my-schedule';
+  const start = DateTime.utc();
+  const event = generators.scheduledEvent({ schedule });
+  const time = DateTime.fromISO(event.time);
+
+  const end = DateTime.utc();
+  test.is(event['detail-type'], 'Scheduled Event');
+  test.is(event.source, 'aws.events');
+  test.true(time.isValid);
+  test.true(time >= start);
+  test.true(time <= end);
+  test.deepEqual(event.resources, [ schedule ]);
 });
